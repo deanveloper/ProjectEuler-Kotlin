a2c1e74798eb630245eb8f97430a72c67f2398ab
Dean B <dean@deanveloper.com>
2016-10-03 01:29:30 -0400

    imported guava, use LongMath to get square root, and fix solution3! :)

--- src/main/java/com/deanveloper/projecteulerkotlin/solutions/Solution3.kt
+++ src/main/java/com/deanveloper/projecteulerkotlin/solutions/Solution3.kt
@@ -2,6 +2,8 @@ package com.deanveloper.projecteulerkotlin.solutions
 
 import com.deanveloper.projecteulerkotlin.Solution
 import com.deanveloper.projecteulerkotlin.runPrimeSieve
+import com.google.common.math.LongMath
+import java.math.RoundingMode
 
 /**
  * @author Dean
@@ -9,7 +11,13 @@ import com.deanveloper.projecteulerkotlin.runPrimeSieve
 object Solution3 : Solution {
     override fun invoke(): Number {
         val num = 600851475143L
-        val primes = runPrimeSieve(num)
+        val sqrt = LongMath.sqrt(num, RoundingMode.UP)
+
+        if(sqrt > Int.MAX_VALUE) {
+            println("Fuck")
+        }
+
+        val primes = runPrimeSieve(sqrt.toInt()).filter { num % it == 0L }
 
         return primes.last()
     }
