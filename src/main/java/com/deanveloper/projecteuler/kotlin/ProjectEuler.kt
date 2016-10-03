adf819f81cb816b956b4b679ddb6180ac2679e55
Dean B <dean@deanveloper.com>
2016-10-03 00:26:01 -0400

    Initial commit

---
+++ src/main/java/com/deanveloper/projecteuler/kotlin/ProjectEuler.kt
@@ -0,0 +1,26 @@
+package com.deanveloper.projecteuler.kotlin
+
+import java.math.BigInteger
+
+/**
+ * @author Dean
+ */
+fun main(args: Array<String>) {
+
+}
+
+interface Problem {
+    fun solve(): String
+}
+
+fun runSieve(primes: Int): IntArray {
+
+}
+
+fun runSieve(primes: Long): LongArray {
+
+}
+
+fun runSieve(primes: BigInteger): Array<BigInteger> {
+
+}
